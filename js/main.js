f889bfb9f67467ad3e0c22864e4c9f0e7e484859
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-02-09 17:29:16 +0000

    API now working with 2 drinks

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.querySelector('button').addEventListener('click', getDrink)
+document.getElementById('button1').addEventListener('click', getDrinks1)
 
-function getDrink(){
-    let drink = document.getElementById('input').value
+function getDrinks1(){
+    let drink1 = document.getElementById('input1').value
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink1}`)
 
 
     .then((res) => res.json()) // parse response as JSON
     .then(data =>  {
         console.log(data.drinks)
-        document.querySelector('h2').innerText = data.drinks[0].strDrink
-        document.querySelector('img').src = data.drinks[0].strDrinkThumb
-        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        document.getElementById('cocktail1').innerText = data.drinks[0].strDrink
+        document.getElementById('img1').src = data.drinks[0].strDrinkThumb
+        document.getElementById('Instructions1').innerText = data.drinks[0].strInstructions
+
     })
     .catch(err => {
         console.log(`error ${err}`)
     })
+};
+
+document.getElementById('button2').addEventListener('click', getDrinks2)
+
+function getDrinks2() {
+    
+    let drink2 = document.getElementById('input2').value
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink2}`)
+
+
+        .then((res) => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data.drinks)
+            document.getElementById('cocktail2').innerText = data.drinks[1].strDrink
+            document.getElementById('img2').src = data.drinks[1].strDrinkThumb
+            document.getElementById('Instructions2').innerText = data.drinks[1].strInstructions
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 };
